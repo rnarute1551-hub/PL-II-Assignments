37bcd1116892769123056e275502b4022a128962
rnarute1551-hub <rnarute1551@gmail.com>
2026-04-23 08:03:11 +0530

    Create Assignment 1.py

---
+++ Assignment 1.py
@@ -0,0 +1,31 @@
+ # Integer
+a = 10
+print("Integer:", a, type(a))
+
+# Float
+b = 3.14
+print("Float:", b, type(b))
+
+# String
+c = "Hello"
+print("String:", c, type(c))
+
+# Boolean
+d = True
+print("Boolean:", d, type(d))
+
+# List
+e = [1, 2, 3]
+print("List:", e, type(e))
+
+# Tuple
+f = (1, 2, 3)
+print("Tuple:", f, type(f))
+
+# Dictionary
+g = {"name": "Aarti", "age": 18}
+print("Dictionary:", g, type(g))
+
+# Set
+h = {1, 2, 3}
+print("Set:", h, type(h))
